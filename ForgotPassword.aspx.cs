38dc6155983c4cab69b715554bdeab2bae5b25d3
Nobir <25162441+nobir@users.noreply.github.com>
2025-04-22 01:41:39 +0600

    [Update] Format and removing unnecessary imports

--- ForgotPassword.aspx.cs
+++ ForgotPassword.aspx.cs
@@ -1,9 +1,9 @@
 using System;
+using System.Collections.Generic;
 using System.Data;
 using System.Data.SqlClient;
 using System.Net.Mail;
 using System.Web;
-using System.Collections.Generic;
 
 namespace Accounts.Xinator
 {
--- ResetPassword.aspx.cs
+++ ResetPassword.aspx.cs
@@ -1,7 +1,7 @@
 using System;
+using System.Collections.Generic;
 using System.Data;
 using System.Data.SqlClient;
-using System.Collections.Generic;
 
 namespace Accounts.Xinator
 {
